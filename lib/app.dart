4012d7cb41f9b55a7584e1a42cb7f71a3a1413a0
shekhrozz <110655114+shekhrozz@users.noreply.github.com>
2022-12-12 17:02:32 +0500

    Changes to be committed: 	modified:   android/app/build.gradle 	new file:   lib/app.dart 	modified:   lib/main.dart 	new file:   lib/pages/home/home_pages.dart 	new file:   lib/utils/app_setup/app_setup.dart 	new file:   lib/utils/theme/dark_theme.dart 	new file:   lib/utils/theme/themes.dart 	new file:   lib/utils/theme/white_theme.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml

---
+++ lib/app.dart
@@ -0,0 +1,31 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recipe_app/pages/home/home_pages.dart';
+import 'package:recipe_app/utils/theme/themes.dart';
+
+class AppProvider extends StatelessWidget {
+  const AppProvider({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+      light: AppTheme.whiteTheme,
+       initial: AppTheme.currentSavedTheme??AdaptiveThemeMode.dark,
+        builder:(light, dark) => MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:HomePage()
+     // HomePage()
+      //  MultiBlocProvider(
+      //   providers:[
+      //    BlocProvider(create:(context) => ,)
+      //   ],
+      //    child: HomePage())
+    )
+  );
+  }
+}
---
+++ lib/utils/app_setup/app_setup.dart
@@ -0,0 +1,17 @@
+
+
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+
+class AppSetup {
+static Future<void>get setup async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    
+   // options:  DefaultFirebaseOptions.currentPlatform,
+  );
+}
+
+}
