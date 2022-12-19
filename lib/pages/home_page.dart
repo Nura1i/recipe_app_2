14b29d8b0c17c546b550ae026ad8fc2916f4079d
Nura1i <110654860+Nura1i@users.noreply.github.com>
2022-12-19 15:59:56 +0500

    Changes to be committed: 	modified:   lib/pages/home_page.dart
    Changes not staged for commit:
    	modified:   lib/app.dart
    	modified:   lib/blocs/login/sign_up_cubit.dart
    	modified:   lib/pages/bookmark_page/bookmark_page.dart
    	modified:   lib/pages/profile_page.dart
    	deleted:    lib/repositories/fire_service.dart
    	modified:   lib/views/home_view.dart
    	modified:   pubspec.lock
    	modified:   pubspec.yaml
    
    Untracked files:
    	lib/blocs/card block/
    	lib/models/Card Model/
    	lib/pages/post_page.dart
    	lib/repositories/services/
    	lib/views/item_of_post.dart
    	lib/widgets/card_view_widget.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,11 @@
-import 'dart:ui';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:recipe_app/blocs/card%20block/list_post_cubit.dart';
+import 'package:recipe_app/blocs/card%20block/list_post_state.dart';
+import 'package:recipe_app/models/Card%20Model/card_http.dart';
+import 'package:recipe_app/views/home_view.dart';
+import 'package:recipe_app/widgets/card_view_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,218 +14,40 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+var posts;
+
 class _HomePageState extends State<HomePage> {
-  var currentUser = FirebaseAuth.instance.currentUser;
+  void initState() {
+    super.initState();
+    BlocProvider.of<ListPostCubit>(context).apiPostList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 64, right: 146),
-              child: Text(
-                "Find best ricepes\for cooking",
-                style: TextStyle(
-                  color: Color(0xFF303030),
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 28, right: 20, left: 20,bottom: 20),
-              child: Container(
-                width: size.width / 0.7.w,
-                height: size.height / 13.h,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(20)),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(top: 13),
-                    hintText: 'Search recipes',
-                    isDense: false,
-                    border: InputBorder.none,
-                    prefixIcon: Icon(Icons.search),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                  physics: BouncingScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  // scrollDirection: Axis.vertical,
-                  child: Column(children: [
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 20, right: 20, top: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Trending Now🔥',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              const Text(
-                                'See All',
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.red),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: SvgPicture.asset(
-                                  'assets/svg/Belgi.svg',
-                                  width: 30,
-                                  height: 20,
-                                ),
-                                color: Colors.red,
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    SingleChildScrollView(
-                        physics: const AlwaysScrollableScrollPhysics(),
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 200,
-                              width: 100,
-                              color: Colors.red,
-                            ),
-                            const SizedBox(
-                              width: 100,
-                            ),
-                            Container(
-                              height: 200,
-                              width: 100,
-                              color: Colors.red,
-                            ),
-                            const SizedBox(
-                              width: 100,
-                            ),
-                            Container(
-                              height: 200,
-                              width: 100,
-                              color: Colors.red,
-                            ),
-                            const SizedBox(
-                              width: 100,
-                            ),
-                            Container(
-                              height: 200,
-                              width: 100,
-                              color: Colors.red,
-                            ),
-                          ],
-                        )),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 20, right: 20, top: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'Trending Now🔥',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              const Text(
-                                'See All',
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.red),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: SvgPicture.asset(
-                                  'assets/svg/Belgi.svg',
-                                  width: 30,
-                                  height: 20,
-                                ),
-                                color: Colors.red,
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    SingleChildScrollView(
-                        physics: const AlwaysScrollableScrollPhysics(),
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 200,
-                              width: 100,
-                              color: Colors.red,
-                            ),
-                            const SizedBox(
-                              width: 100,
-                            ),
-                            Container(
-                              height: 200,
-                              width: 100,
-                              color: Colors.red,
-                            ),
-                            const SizedBox(
-                              width: 100,
-                            ),
-                            Container(
-                              height: 200,
-                              width: 100,
-                              color: Colors.red,
-                            ),
-                            const SizedBox(
-                              width: 100,
-                            ),
-                            Container(
-                              height: 200,
-                              width: 100,
-                              color: Colors.red,
-                            ),
-                          ],
-                        )),
-                    ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      scrollDirection: Axis.vertical,
-                      itemCount: 20,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            color: Colors.orange,
-                            width: 100,
-                            height: 100,
-                          ),
-                        );
-                      },
-                    )
-                  ])),
-            )
-          ],
-        ),
+      body: BlocBuilder<ListPostCubit, ListPostState>(
+        builder: (BuildContext context, ListPostState state) {
+          if (state is ListPostLoaded) {
+            posts = state.posts!;
+            // builder(posts);
+            return const HomeView();
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
+}
+
+Widget builder(List<Post> items) {
+  return ListView.builder(
+    scrollDirection: Axis.horizontal,
+    itemCount: items.length,
+    itemBuilder: (contex, index) {
+      return cardView(contex, posts[index]);
+    },
+  );
 }
